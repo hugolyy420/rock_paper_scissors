b05339ce3ce7221e67ddcd9345b1a7b6b510b772
Hugo Leung <132837475+hugounique@users.noreply.github.com>
2023-07-29 16:05:26 -0400

    Add an event listener for the buttons

--- javascript.js
+++ javascript.js
@@ -1,36 +1,39 @@
+//Add btn elements into variables
+const btns = document.querySelectorAll("button");
+let playerScore = 0;
+let computerScore = 0;
+
 let options = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice () {
     return (options[Math.floor(Math.random() * options.length)]);
 }
-let playerScore = 0;
-let computerScore = 0;
 
 function playRound (playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
         playerScore++;
-        return (`You win! Rock beats scissors. Player's score : ${playerScore} and computer's score : ${computerScore}`)
+         console.log(`You win! Rock beats scissors. Player's score : ${playerScore} and computer's score : ${computerScore}`)
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
         playerScore++;
-        return (`You win! Paper beats rock. Player's score : ${playerScore} and computer's score : ${computerScore}`) 
+        console.log (`You win! Paper beats rock. Player's score : ${playerScore} and computer's score : ${computerScore}`) 
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
         playerScore++;
-        return (`You win! Scissors beats paper. Player's score : ${playerScore} and computer's score : ${computerScore}`)
+        console.log (`You win! Scissors beats paper. Player's score : ${playerScore} and computer's score : ${computerScore}`)
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
         computerScore++;
-        return (`You lose! Paper beats rock. Player's score : ${playerScore} and computer's score : ${computerScore}`)   
+        console.log (`You lose! Paper beats rock. Player's score : ${playerScore} and computer's score : ${computerScore}`)   
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
         computerScore++;
-        return (`You lose! Scissors beats paper. Player's score : ${playerScore} and computer's score : ${computerScore}`)   
+        console.log (`You lose! Scissors beats paper. Player's score : ${playerScore} and computer's score : ${computerScore}`)   
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
         computerScore++;
-        return (`You lose! Rock beats scissors. Player's score : ${playerScore} and computer's score : ${computerScore}`)   
+        console.log (`You lose! Rock beats scissors. Player's score : ${playerScore} and computer's score : ${computerScore}`)   
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "rock") {
-        return (`It is a tie! Rock ties with rock. Player's score : ${playerScore} and computer's score : ${computerScore}`)     
+        console.log (`It is a tie! Rock ties with rock. Player's score : ${playerScore} and computer's score : ${computerScore}`)     
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper") {
-        return (`It is a tie! Paper ties with paper. Player's score : ${playerScore} and computer's score : ${computerScore}`)
+        console.log (`It is a tie! Paper ties with paper. Player's score : ${playerScore} and computer's score : ${computerScore}`)
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "scissors") {
-        return (`It is a tie! Scissors ties with scissors. Player's score : ${playerScore} and computer's score : ${computerScore}`)
+        console.log (`It is a tie! Scissors ties with scissors. Player's score : ${playerScore} and computer's score : ${computerScore}`)
     }
 }
 
@@ -38,11 +41,6 @@ function game () {
     console.log(playRound (prompt("Rock, Paper or Scissors?"), getComputerChoice()));
 } 
 
-game ();
-game ();
-game ();
-game ();
-game ();
 
 function getFinalResult () {
     if (playerScore > computerScore) {
@@ -54,4 +52,14 @@ function getFinalResult () {
     }
 }
 
-getFinalResult ();
+btns.forEach((btn) => {
+
+    btn.addEventListener("click", function(e) {
+        playRound(e.target.innerText, getComputerChoice ());
+    })
+})
+
+// Add event listener to hear the click event and fire off the playRound function
+// How to get the choice into the playerSelection argument?
+
+// getFinalResult ();
