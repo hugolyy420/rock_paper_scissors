45e4bc16f8803bbfec5ad2ee7e890faeb5b715fe
Hugo Leung <132837475+hugounique@users.noreply.github.com>
2023-07-30 14:21:51 -0400

    Finish setting up display of written result

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
 //Add btn elements into variables
 const btns = document.querySelectorAll("button");
-const div = document.createElement("div");
+const firstResultAnnouncer = document.querySelector("#intro-container").firstElementChild;
+const secondResultAnnouncer = document.querySelector("#intro-container").lastElementChild;
+const playerScoreAnnouncer = document.querySelector('#player-score');
+const computerScoreAnnouncer = document.querySelector('#computer-score');
+
 let playerScore = 0;
 let computerScore = 0;
 
@@ -13,34 +17,61 @@ function getComputerChoice () {
 function playRound (playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
         playerScore++;
-         div.textContent = `You win! Rock beats scissors. Player's score : ${playerScore} and computer's score : ${computerScore}`
+        playerScoreAnnouncer.textContent = `Player's score : ${playerScore}`
+        computerScoreAnnouncer.textContent = `Computer's score : ${computerScore}`
+        firstResultAnnouncer.textContent = "You win!"
+        secondResultAnnouncer.textContent = "Rock beats scissors." 
          checkResult()
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
         playerScore++;
-        div.textContent = `You win! Paper beats rock. Player's score : ${playerScore} and computer's score : ${computerScore}`
+        playerScoreAnnouncer.textContent = `Player's score : ${playerScore}`
+        computerScoreAnnouncer.textContent = `Computer's score : ${computerScore}`
+        firstResultAnnouncer.textContent = "You win!"
+        secondResultAnnouncer.textContent = "Paper beats rock." 
         checkResult()
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
         playerScore++;
-        div.textContent = `You win! Scissors beats paper. Player's score : ${playerScore} and computer's score : ${computerScore}`
+        playerScoreAnnouncer.textContent = `Player's score : ${playerScore}`
+        computerScoreAnnouncer.textContent = `Computer's score : ${computerScore}`
+        firstResultAnnouncer.textContent = "You win!"
+        secondResultAnnouncer.textContent = "Scissors beats paper." 
         checkResult()
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
         computerScore++;
-        div.textContent = `You lose! Paper beats rock. Player's score : ${playerScore} and computer's score : ${computerScore}`  
+        playerScoreAnnouncer.textContent = `Player's score : ${playerScore}`
+        computerScoreAnnouncer.textContent = `Computer's score : ${computerScore}`
+        firstResultAnnouncer.textContent = "You lose..."
+        secondResultAnnouncer.textContent = "Paper beats rock." 
         checkResult()
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
         computerScore++;
-        div.textContent = `You lose! Scissors beats paper. Player's score : ${playerScore} and computer's score : ${computerScore}`
+        playerScoreAnnouncer.textContent = `Player's score : ${playerScore}`
+        computerScoreAnnouncer.textContent = `Computer's score : ${computerScore}`
+        firstResultAnnouncer.textContent = "You lose..."
+        secondResultAnnouncer.textContent = "Scissors beats paper." 
         checkResult()
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
         computerScore++;
-        div.textContent = `You lose! Rock beats scissors. Player's score : ${playerScore} and computer's score : ${computerScore}`
+        playerScoreAnnouncer.textContent = `Player's score : ${playerScore}`
+        computerScoreAnnouncer.textContent = `Computer's score : ${computerScore}`
+        firstResultAnnouncer.textContent = "You lose..."
+        secondResultAnnouncer.textContent = "Rock beats scissors." 
         checkResult()
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "rock") {
-        div.textContent = `It is a tie! Rock ties with rock. Player's score : ${playerScore} and computer's score : ${computerScore}`
+        playerScoreAnnouncer.textContent = `Player's score : ${playerScore}`
+        computerScoreAnnouncer.textContent = `Computer's score : ${computerScore}`
+        firstResultAnnouncer.textContent = "It is a tie!"
+        secondResultAnnouncer.textContent = "Rock ties with rock." 
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper") {
-        div.textContent = `It is a tie! Paper ties with paper. Player's score : ${playerScore} and computer's score : ${computerScore}`
+        playerScoreAnnouncer.textContent = `Player's score : ${playerScore}`
+        computerScoreAnnouncer.textContent = `Computer's score : ${computerScore}`
+        firstResultAnnouncer.textContent = "It is a tie!"
+        secondResultAnnouncer.textContent = "Paper ties with paper." 
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "scissors") {
-        div.textContent = `It is a tie! Scissors ties with scissors. Player's score : ${playerScore} and computer's score : ${computerScore}`
+        playerScoreAnnouncer.textContent = `Player's score : ${playerScore}`
+        computerScoreAnnouncer.textContent = `Computer's score : ${computerScore}`
+        firstResultAnnouncer.textContent = "It is a tie!"
+        secondResultAnnouncer.textContent = "Scissors ties with scissors." 
     } 
 }
 
@@ -67,14 +98,6 @@ function getFinalResult () {
 btns.forEach((btn) => {
 
     btn.addEventListener("click", function(e) {
-        playRound(e.target.innerText, getComputerChoice ());
+        playRound(e.target.id, getComputerChoice ());
     })
 })
-
-document.body.appendChild(div);
-
-
-// Add event listener to hear the click event and fire off the playRound function
-// How to get the choice into the playerSelection argument?
-
-// getFinalResult ();
